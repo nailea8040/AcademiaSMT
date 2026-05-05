39d725571bc9f1c26efa02d1da69f0927d6882d8
Nailea Garcia Tencos <nailea8040@gmail.com>
2026-05-04 21:39:41 -0600

    4ta prueba para pagos en linea.

--- config/services.php
+++ config/services.php
@@ -35,4 +35,12 @@ return [
         ],
     ],
 
+    // config/services.php
+
+    'mercadopago' => [
+        'access_token' => env('MP_ACCESS_TOKEN'),
+        'public_key'   => env('MP_PUBLIC_KEY'),
+    ],
+
+
 ];
