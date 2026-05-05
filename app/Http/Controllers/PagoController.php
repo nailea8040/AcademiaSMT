cac42af8b15f005904fde28f6757a043b27e6212
Nailea Garcia Tencos <nailea8040@gmail.com>
2026-05-04 21:25:56 -0600

    3era prueba para pagos en linea.

--- app/Http/Controllers/PagoController.php
+++ app/Http/Controllers/PagoController.php
@@ -38,8 +38,6 @@ class PagoController extends Controller
 
             $tipos_pago = DB::table('tipo_pago')->orderBy('id_tipo_pago')->get();
 
-            dd($alumnos, $tipos_pago);
-            
             return view('pagosViews.pagos', compact('pagos', 'alumnos', 'tipos_pago'));
 
         } catch (\Exception $e) {
