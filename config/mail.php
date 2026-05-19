d7eeb78ccaa01bc1db13f6e1581610cc311512ad
Nailea Garcia Tencos <nailea8040@gmail.com>
2026-05-19 03:00:38 -0600

    Correccion de recuperacion de contraseña 6

--- config/mail.php
+++ config/mail.php
@@ -48,13 +48,13 @@ return [
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
             'stream' => [
-            'ssl' => [
-                'allow_self_signed' => true,
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-            ],
-        ],
-        ],
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ], // <-- Primero se cierra el arreglo 'stream'
+        ], // <-- Luego se cierra el arreglo 'smtp'
 
         'ses' => [
             'transport' => 'ses',
