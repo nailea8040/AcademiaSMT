198fce30e4d6ac7c74a069eb371c310ca74e8083
Nailea Garcia Tencos <nailea8040@gmail.com>
2026-07-16 22:01:15 -0600

    fix: alinear contenedores div root y agregar viteReactRefresh

--- resources/views/torneo.blade.php
+++ resources/views/torneo.blade.php
@@ -1,6 +1,13 @@
 @extends('includes.menu')
 
 @section('content')
-<div id="torneo-app"></div>
+<!-- Colocamos ambos IDs comunes para asegurar que React enganche sin importar cuál use tu main.jsx -->
+<div id="torneo-app">
+    <div id="torneo-root">
+        <div id="root"></div>
+    </div>
+</div>
+
+@viteReactRefresh
 @vite(['resources/js/torneo/main.jsx'])
 @endsection
