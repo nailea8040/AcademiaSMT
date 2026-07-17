1d2af4a7a6d89a2bf3b31682e3b19eabb3a0099d
Nailea Garcia Tencos <nailea8040@gmail.com>
2026-07-16 22:23:57 -0600

    fix: reestructurar vista para inyectar correctamente el menu y montar react

--- resources/views/torneo.blade.php
+++ resources/views/torneo.blade.php
@@ -1,8 +1,19 @@
-@extends('includes.menu')
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Torneos | Academia Karate-Do SMT</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="{{ asset('css/estilo2.css') }}">
+    @viteReactRefresh
+    @vite(['resources/js/torneo/main.jsx'])
+</head>
+<body>
+    @include('includes.menu')
 
-@section('content')
-<div id="torneo-app"></div>
-
-@viteReactRefresh
-@vite(['resources/js/torneo/main.jsx'])
-@endsection
+    <div class="main-content">
+        <div id="torneo-app"></div>
+    </div>
+</body>
+</html>
