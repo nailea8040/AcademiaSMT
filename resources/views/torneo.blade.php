5fcb6dcab824abc47e6588921d9cb1c95efa8986
Nailea Garcia Tencos <nailea8040@gmail.com>
2026-07-16 22:16:46 -0600

    fix: usar directiva vite para resolver hashes de produccion dinamicamente

--- resources/views/torneo.blade.php
+++ resources/views/torneo.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div id="torneo-app"></div>
 
-<!-- FORZAMOS LA RUTA DEL JS COMPILADO -->
-<script type="module" src="{{ asset('build/assets/main-DKSnEJt5.js') }}"></script>
-<link rel="stylesheet" href="{{ asset('build/assets/app-CtW5FsCk.css') }}">
+@viteReactRefresh
+@vite(['resources/js/torneo/main.jsx'])
 @endsection
